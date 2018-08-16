c417a83e8d02e807c8decc352a56db1f62600cae
whsgzcy <whsgzcy@foxmail.com>
2018-08-16 18:56:01 +0800

    list 去重

---
+++ src/com/iwant/Tools.java
@@ -0,0 +1,111 @@
+package com.iwant;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Tools {
+	
+	
+	public static void main(String[] args) {
+		
+		// 两个循环 一个对象 找出交集
+		List<A> la = new ArrayList<>();
+		for(int i = 0; i < 8; i++){
+			A a = new A();
+			a.setName("name" + i);
+			la.add(a);
+		}
+	
+		List<B> lb = new ArrayList<>();
+		for(int i = 0; i < 16; i++){
+			B b = new B();
+			b.setName("name" + i);
+			if(i > 8){
+				b.setName2("name" + i);
+			}else{
+				b.setName2("name" + i);
+			}
+			lb.add(b);
+		}
+		
+		String name = "name6";
+		
+		// 找出交集
+		List<String> names = new ArrayList<>();
+		for(int i = 0; i < la.size(); i++){
+			String aName = la.get(i).getName();
+			for(int m = 0; m < lb.size(); m++){
+				String bName = lb.get(m).getName();
+				String b2Name = lb.get(m).getName2();
+				if(aName.equals(bName) && aName.equals(b2Name) && aName.equals(name)){
+					System.out.println("name ---> " + aName);
+				}
+			}
+		}
+		
+		List<String> lc = new ArrayList<>();
+		lc.add("a");
+		lc.add("b");
+		lc.add("c");
+		lc.add("g");
+		List<String> ld = new ArrayList<>();
+		ld.add("a");
+		ld.add("b");
+		ld.add("c");
+		ld.add("d");
+		ld.add("e");
+		ld.add("f");		
+
+		lc.addAll(ld);
+		
+		List<String> b = removeDuplicteUsers(lc);
+		System.out.println(b);
+	}
+	
+	 public static List<String> removeDuplicteUsers(List<String> list) {
+	        Set<String> s = new TreeSet<String>(new Comparator<String>() {
+
+	            @Override
+	            public int compare(String o1, String o2) {
+	                return o1.compareTo(o2);
+	            }
+	        });
+	        s.addAll(list);
+	        return new ArrayList<String>(s);
+	    }
+	
+	static class A{
+		String name;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+	}
+	
+	static class B{
+		String name;
+		String name2;
+		
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public String getName2() {
+			return name2;
+		}
+		public void setName2(String name2) {
+			this.name2 = name2;
+		}
+		
+	}
+
+}
--- src/com/iwant/threa/HandleThread.java
+++ src/com/iwant/threa/HandleThread.java
@@ -5,23 +5,23 @@ import java.util.concurrent.TimeUnit;
 
 public class HandleThread {
 	
-//	static Thread t1 = new Thread(){
-//		public void run() {
-//			System.out.println("thread ---> " + "t1");
-//		};
-//	};
-//	
-//	static Thread t2 = new Thread(){
-//		public void run() {
-//			System.out.println("thread ---> " + "t2");
-//		};
-//	};
-//	
-//	static Thread t3 = new Thread(){
-//		public void run() {
-//			System.out.println("thread ---> " + "t3");
-//		};
-//	};
+	static Thread t1 = new Thread(){
+		public void run() {
+			System.out.println("thread ---> " + "t1");
+		};
+	};
+	
+	static Thread t2 = new Thread(){
+		public void run() {
+			System.out.println("thread ---> " + "t2");
+		};
+	};
+	
+	static Thread t3 = new Thread(){
+		public void run() {
+			System.out.println("thread ---> " + "t3");
+		};
+	};
 	
 	static Thread t4 = new Thread(){
 		public void run() {
@@ -71,10 +71,16 @@ public class HandleThread {
 	
 	public static void main(String[] args) {
 //		ScheduledThreadPoolExecutor test = new ScheduledThreadPoolExecutor(3);
-//		test.scheduleAtFixedRate(t1, 1, 5, TimeUnit.SECONDS);
+//		test.scheduleAtFixedRate(t1, 1, 1, TimeUnit.SECONDS);
 //		test.scheduleAtFixedRate(t2, 1, 5, TimeUnit.SECONDS);
 //		test.scheduleAtFixedRate(t3, 1, 5, TimeUnit.SECONDS);
 		
-		t4.start();
+		int code1 = 12;
+		int code2 = 34;
+		int code3 = 79;
+		String code = code1 / 10 + "" + code1 % 10 + "" + code2 / 10 + "" + code2 % 10 + "" + code3 / 10 + "" + code3 % 10 + "";
+		System.out.println(code + "");
+		
+//		t4.start();
 	}
 }
